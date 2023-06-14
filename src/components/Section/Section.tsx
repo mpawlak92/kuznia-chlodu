f915c6ace463b2f547c575c3bd76f7f66337b35d
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-14 21:33:18 +0200

    added wrapper to section component

--- src/components/Section/Section.tsx
+++ src/components/Section/Section.tsx
@@ -10,7 +10,7 @@ type propsType = {
 const Section = ({ id, bgcolor, children }: propsType): JSX.Element => {
   return (
     <SectionContainer bgcolor={bgcolor} id={id}>
-      {children}
+      <Wrapper bgcolor={bgcolor}>{children}</Wrapper>
     </SectionContainer>
   );
 };
@@ -19,7 +19,20 @@ export default Section;
 // Pick<propsType, 'bgcolor'>
 const SectionContainer = styled.section<propsType>`
   display: flex;
-  max-width: 100vw;
-  height: 100vh;
-  background-color: ${({ bgcolor }) => bgcolor || 'white'};
+  flex-wrap: wrap;
+  justify-content: center;
+  width: 100%;
+  background-color: ${(props) =>
+    props.bgcolor ? props.bgcolor : props.theme.palette.primary.main};
+`;
+{
+  /* <Pick<propsType, 'bgcolor'>> */
+}
+const Wrapper = styled.div<Pick<propsType, 'bgcolor'>>`
+  display: flex;
+  width: 90%;
+  margin: 2rem 0;
+  max-width: 1600px;
+  background-color: ${(props) =>
+    props.bgcolor ? props.bgcolor : props.theme.palette.primary.main};
 `;
