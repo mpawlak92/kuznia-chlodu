e39a6f9c4747bbed92fc2e58fde992a555daa5f7
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-14 15:07:14 +0200

    sets basic routing

---
+++ src/Routing/Routing.tsx
@@ -0,0 +1,28 @@
+import { Route, Routes } from 'react-router-dom';
+import Section from '../components/Section/Section';
+import ScrolledPage from '../Layout/ScrolledPage';
+import Contact from '../Layout/Contact';
+
+const Routing = () => {
+  return (
+    <>
+      <Routes> 
+        <Route path='/' element={<ScrolledPage />} />
+        <Route
+          path='/klimatyzacja-domowa'
+          element={<Section bgcolor='royalblue' id='uslugi' />}
+        />
+        <Route
+          path='/klimatyzacja-mobilna'
+          element={<Section bgcolor='royalblue' id='uslugi' />}
+        />
+        <Route
+          path='/czyszczenie-klimatyzacji'
+          element={<Section bgcolor='royalblue' id='uslugi' />}
+        />
+        <Route path='/kontakt' element={<Contact />} />
+      </Routes>
+    </>
+  );
+};
+export default Routing;
