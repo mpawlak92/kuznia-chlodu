961e19489da2ba84ae65de73cf11476e85c8a54a
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-14 15:07:01 +0200

    createted draft of contact page

---
+++ src/Layout/Contact.tsx
@@ -0,0 +1,15 @@
+import Menu from '../components/Menu/Menu';
+import Section from '../components/Section/Section';
+
+const Contact = () => {
+  return (
+    <>
+      <Menu />
+      <Section bgcolor='white' id='contact'>
+        <p>contact</p>
+      </Section>
+    </>
+  );
+};
+
+export default Contact;
