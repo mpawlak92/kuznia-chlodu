72f595187a360b362d1ab23737a736163bfd0c7c
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-15 16:57:48 +0200

    feat: added hover and on click behavior for card

--- src/Layout/Services.tsx
+++ src/Layout/Services.tsx
@@ -2,27 +2,65 @@ import styled from 'styled-components';
 import homeImg from '../assets/service_home.jpg';
 import mobileImg from '../assets/service_mobile.jpg';
 import serviceImg from '../assets/service_service.jpg';
+import { Link } from 'react-router-dom';
 
 const Services = () => {
   return (
     <SecrionContainer>
       <SectionHeading>Nasze usługi</SectionHeading>
-      <ServiceCard img={homeImg}>
-        <ServiceCard_cover>
+
+      <ServiceCard>
+        <ServiceCard_cover img={homeImg}>
           <ServiceCard_textBelt>Do domu i biura</ServiceCard_textBelt>
         </ServiceCard_cover>
+        <ServiceCard_secondSide>
+          <Link className='link' to='/domowa'>
+            <h2>Do domu i biura</h2>
+            <p>
+              Dobierzemy, przywieziemy, zamontujemy i będziemy dbać o kondycję
+              Twojej klimatyzacji.
+            </p>
+            <p className='fake-link'>
+              Kliknij w kartę, aby dowiedzieć się wiecej.
+            </p>
+          </Link>
+        </ServiceCard_secondSide>
       </ServiceCard>
 
-      <ServiceCard img={mobileImg}>
-        <ServiceCard_cover>
-          <ServiceCard_textBelt>Samochodowa</ServiceCard_textBelt>
+      <ServiceCard>
+        <ServiceCard_cover img={mobileImg}>
+          <ServiceCard_textBelt>Samochodowa - mobilna</ServiceCard_textBelt>
         </ServiceCard_cover>
+        <ServiceCard_secondSide>
+          <Link className='link' to='/mobilna'>
+            <h2>Samochodowa - mobilna</h2>
+            <p>
+              Dobierzemy, przywieziemy, zamontujemy i będziemy dbać o kondycję
+              Twojej klimatyzacji.
+            </p>
+            <p className='fake-link'>
+              Kliknij w kartę, aby dowiedzieć się wiecej.
+            </p>
+          </Link>
+        </ServiceCard_secondSide>
       </ServiceCard>
 
-      <ServiceCard img={serviceImg}>
-        <ServiceCard_cover>
+      <ServiceCard>
+        <ServiceCard_cover img={serviceImg}>
           <ServiceCard_textBelt>Serwis</ServiceCard_textBelt>
         </ServiceCard_cover>
+        <ServiceCard_secondSide>
+          <Link className='link' to='/serwis'>
+            <h2>Serwis</h2>
+            <p>
+              Dobierzemy, przywieziemy, zamontujemy i będziemy dbać o kondycję
+              Twojej klimatyzacji.
+            </p>
+            <p className='fake-link'>
+              Kliknij w kartę, aby dowiedzieć się wiecej.
+            </p>
+          </Link>
+        </ServiceCard_secondSide>
       </ServiceCard>
     </SecrionContainer>
   );
@@ -45,35 +83,22 @@ const SectionHeading = styled.h2`
   text-align: center;
   color: ${(props) => props.theme.palette.secondary.contrastText};
 `;
-const ServiceCard = styled.button<{ img: string }>`
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+
+const ServiceCard_cover = styled.div<{ img: string }>`
+  position: relative;
   width: 100%;
-  height: auto;
-  min-width: 330px;
-  max-width: 450px;
-  min-height: 350px;
-  margin: 2rem 1rem;
+  height: 100%;
   background-image: url(${(props) => props.img});
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
-  border: none;
   border-radius: 8px;
   box-shadow: 1px 1px 8px 1px rgba(0, 0, 0, 0.3);
-  cursor: pointer;
-`;
-const ServiceCard_cover = styled.div`
-  position: relative;
-  width: 100%;
-  height: 100%;
-  background-image: linear-gradient(
-    25deg,
-    rgba(0, 0, 0, 0.3) 0%,
-    transparent 85%
-  );
-  border-radius: 8px;
+  transition: 0.8s;
+
+  &:hover {
+    transform: rotateY(180deg);
+  }
 `;
 const ServiceCard_textBelt = styled.div`
   position: absolute;
@@ -86,7 +111,74 @@ const ServiceCard_textBelt = styled.div`
   height: 30%;
   font-size: 3rem;
   color: ${(props) => props.theme.palette.secondary.contrastText};
-  background-color: ${(props) => props.theme.palette.secondary.mainSemiTransparent};
+  background-color: ${(props) =>
+    props.theme.palette.secondary.mainSemiTransparent};
   border-bottom-left-radius: 8px;
   border-bottom-right-radius: 8px;
+`;
+const ServiceCard_secondSide = styled.div`
+  position: relative;
+  top: -100%;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  background-color: #fefefe;
+  background-image: linear-gradient(
+    315deg,
+    ${(props) => props.theme.palette.common.white} 0%,
+    ${(props) => props.theme.palette.common.primary} 74%
+  );
+  border-radius: 8px;
+  h2 {
+    margin-bottom: 0.2em;
+    font-size: 1em;
+  }
+  transition: 0.8s;
+  opacity: 0;
+  transform: rotateY(-180deg);
+
+  .link {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-content: center;
+    padding: 1rem;
+    font-size: 3rem;
+    color: ${(props) => props.theme.palette.common.black};
+    text-decoration: none;
+  }
+  .fake-link {
+    font-size: 1.5rem;
+    margin: 0.6rem;
+    color: purple;
+  }
+`;
+const ServiceCard = styled.button`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  width: 100%;
+  height: auto;
+  min-width: 330px;
+  max-width: 450px;
+  min-height: 350px;
+  margin: 2rem 1rem;
+  border: none;
+  border-radius: 8px;
+  perspective: 800;
+  cursor: pointer;
+  transition: trasform 0.8s;
+
+  &:hover ${ServiceCard_cover} {
+    transform: rotateY(-180deg);
+  }
+  &:hover ${ServiceCard_secondSide} {
+    transform: rotateY(0deg);
+    opacity: 1;
+  }
 `;
