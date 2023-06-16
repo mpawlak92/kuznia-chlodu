b19d734ec33684048b22816f112bacc5ee1adad2
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-16 10:13:13 +0200

    added brakepoints declaration for media

---
+++ src/style/Brakepoints.ts
@@ -0,0 +1,9 @@
+export const device = {
+  mobileM: `(min-width: 576px)`,
+  mobileL: `(min-width: 768px)`,
+  tablet: `(min-width: 992px)`,
+  laptop: `(min-width: 1200px)`,
+  laptopL: `(min-width: 1400px)`,
+  desktop: `(min-width: 1600px)`,
+  desktopL: `(min-width: 2500px)`,
+};
