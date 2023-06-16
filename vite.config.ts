536ef0c4591ae5ffc1d0c6d6a8a637577719757e
Michal Pawlak <mpawlak.it@gmail.com>
2023-06-16 17:25:43 +0200

    deploy for test 2

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   const config = {
     plugins: [react()],
+    base: '/',
   };
-
-  if (command !== 'serve') {
-    config.base = '/kuznia-chlodu/';
-  }
 
   return config;
 });
